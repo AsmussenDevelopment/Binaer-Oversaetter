cf45e0397add28c5064b276db920a3ae130b3e81
Bastian <76102128+AsmussenDevelopment@users.noreply.github.com>
2021-09-10 13:33:33 +0200

    Create Translator.java

---
+++ Translator.java
@@ -0,0 +1,32 @@
+package dev.asmussen;
+
+public class Translator {
+
+    public static String toText(String bits) {
+
+        String translated = "";
+        char nextChar;
+
+        for(int i = 0; i <= bits.length()-8; i += 8) {
+
+            nextChar = (char) Integer.parseInt(bits.substring(i, i+8), 2);
+            translated += nextChar;
+        }
+
+        return translated;
+
+    }
+
+    public static String toBinary(String text) {
+
+        String translated = "";
+
+        char[] messChar = text.toCharArray();
+
+        for (int i = 0; i < messChar.length; i++) {
+            translated += Integer.toBinaryString(messChar[i]) + " ";
+        }
+
+        return translated;
+    }
+}
